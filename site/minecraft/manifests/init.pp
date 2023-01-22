e6b81df9673af7a75dc2c19f97ec0e24551a2d77
takintunde <121248891+takintunde@users.noreply.github.com>
2023-01-22 20:54:14 +0000

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,25 @@
+class minecraft {
+  file {‘/opt/minecraft’:
+    ensure => directory,
+  }
+  file {‘/opt/minecraft/minecraft_server.1.12.2.jar’:
+    ensure => file,
+    source      => 'https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar',
+  }
+  package {‘java’:
+    ensure => present,
+  }
+  file {‘/opt/minecraft/eula.txt’:
+    ensure => file,
+    content => ‘eula=true’
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
+    ensure => file,
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
+  }
+  service { ‘minecraft’:
+    ensure => running,
+    enable => true,
+  }
+}
+
