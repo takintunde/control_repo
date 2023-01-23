7808b23e2a9c85853f3c275f732cb71be6f121b8
takintunde <121248891+takintunde@users.noreply.github.com>
2023-01-23 22:02:04 +0000

    Delete chocolatey.pp

--- site/profile/manifests/chocolatey.pp
+++
@@ -1,7 +0,0 @@
-class mymodule::chocolatey {
-  package { 'notepadplusplus.settings':
-  ensure   => '1.0.0.20141029',
-  provider => 'chocolatey',
-  source   => 'http://internal/odata/repo',
-  }
-}
