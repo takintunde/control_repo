e33e15a3a70a813f274170e9220cf69c6344b344
takintunde <121248891+takintunde@users.noreply.github.com>
2023-01-23 16:46:10 +0000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -4,6 +4,7 @@ class profile::agent_nodes {
   dockeragent::node { 'db.puppet.vm': }
   dockeragent::node { 'minetest.puppet.vm': }
   dockeragent::node { 'taiwo.puppet.vm': }
+  dockeragent::node { 'chocolate.puppet.vm': }
   host { 'web.puppet.vm':
     ensure => present,
     ip => '172.18.0.2',
