32951b8dea053400061cb9499ddc407395696d83
takintunde <121248891+takintunde@users.noreply.github.com>
2023-01-24 00:13:00 +0000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,10 +1,17 @@
 class profile::agent_nodes {
+  include chocolatey
   include dockeragent
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
   dockeragent::node { 'minetest.puppet.vm': }
   dockeragent::node { 'taiwo.puppet.vm': }
   dockeragent::node { 'chocolate.puppet.vm': }
+  
+  host 'taiwo.puppet.vm':
+    package { 'git':
+      ensure   => 'latest',
+      provider => chocolatey,
+  }
   host { 'web.puppet.vm':
     ensure => present,
     ip => '172.18.0.2',
