851458a89aa7c269416de4302d42cf4bc61bc090
takintunde <121248891+takintunde@users.noreply.github.com>
2023-01-28 15:42:50 +0000

    Update chocolatey_package.pp

--- site/profile/manifests/chocolatey_package.pp
+++ site/profile/manifests/chocolatey_package.pp
@@ -1,6 +1,7 @@
-class profile::chocolatey_package {
-  package { 'Java':
-    ensure   => 'latest',
+class profile::chocolate_server { 
+  package { [ 'Java', 'nginx', 'apache-httpd']:
+    ensure => installed,
     provider => chocolatey,
-  }
-}
+  #$dockercompose_version,          # this is a way to pass a variable
+ }
+ }
